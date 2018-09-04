53b8aab5cfe4a6b0d2ae9ec0830cbc6a0a850f68
santosh <santosh.ece06@gmail.com>
2018-09-04 16:46:46 +0530

    page4 for iframe.

--- LayoutTutorial/src/app/app-routes.ts
+++ LayoutTutorial/src/app/app-routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
 
 export const appRoutes: Routes = [
     {path: 'page1', component: Page1Component},
     {path: 'page2', component: Page2Component},
     {path: 'page3', component: Page3Component},
+    {path: 'page4', component: Page4Component},
     {path: '', redirectTo: '/page1', pathMatch: 'full'}
 ];
 
-export const appComponents = [Page1Component, Page2Component, Page3Component];
+export const appComponents = [Page1Component, Page2Component, Page3Component, Page4Component];
--- LayoutTutorial/src/app/app.module.ts
+++ LayoutTutorial/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { appRoutes, appComponents} from './app-routes';
-import { Page3Component } from './page3/page3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    appComponents,
-    Page3Component
+    appComponents
   ],
   imports: [
     BrowserModule,
